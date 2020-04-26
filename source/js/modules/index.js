e39e838d22b723ab3b042b8ef31673c6974487f1
Roman <rkorupaev@gmail.com>
2020-04-26 15:05:16 +0300

    last fixes

--- source/js/modules/index.js
+++ source/js/modules/index.js
@@ -12,79 +12,45 @@ monthToggler.forEach((toggler) => {
   });
 });
 
-let mySwiper = new Swiper(`.coaches-block__slider`, {
-  // Optional parameters
-  direction: `horizontal`,
-  loop: true,
-  grabCursor: true,
-  speed: 1500,
-  // Navigation arrows
-  navigation: {
-    nextEl: `.swiper-button-next`,
-    prevEl: `.swiper-button-prev`,
-  },
+const coachesBlock = document.querySelector(`.coaches-block__slider`);
 
-  breakpoints: {
-    320: {
-      slidesPerView: 1,
-      spaceBetween: 47,
-      slidesPerGroup: 1
+const mySwiper = (el) => {
+  // eslint-disable-next-line no-undef
+  return new Swiper(el, {
+    // Optional parameters
+    direction: `horizontal`,
+    loop: true,
+    grabCursor: true,
+    speed: 1500,
+    // Navigation arrows
+    navigation: {
+      nextEl: `.swiper-button-next`,
+      prevEl: `.swiper-button-prev`,
     },
-    768: {
-      slidesPerView: 2,
-      spaceBetween: 30,
-      slidesPerGroup: 2
-    },
-    1200: {
-      slidesPerView: 4,
-      spaceBetween: 40,
-      slidesPerGroup: 4
+
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 47,
+        slidesPerGroup: 1
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+        slidesPerGroup: 2
+      },
+      1200: {
+        slidesPerView: 4,
+        spaceBetween: 40,
+        slidesPerGroup: 4
+      }
     }
-  }
-});
+  });
+};
 
-let mySwiper2 = new Swiper(`.reviews-block__slider`, {
-  // Optional parameters
-  direction: `horizontal`,
-  loop: true,
-  grabCursor: true,
-  speed: 1500,
-  // Navigation arrows
-  navigation: {
-    nextEl: `.reviews-block__next`,
-    prevEl: `.reviews-block__prev`,
-  },
-});
-
-let scroll = new SmoothScroll(`a[href*="#"]`, {
-  speed: 500
-});
-
-let phoneInput = document.getElementById(`phone`);
-let phoneMask = IMask(phoneInput, {
-  mask: `+{7}(000)000-00-00`,
-  lazy: false
-});
-
-let form = document.querySelector(`.contacts-block__form-wrapper form`);
-let formValidity = false;
-
-phoneInput.addEventListener(`input`, () => {
-  if (phoneInput.value.indexOf(`_`) !== -1) {
-    phoneInput.setCustomValidity(`Заполните это поле.`);
-  } else {
-    phoneInput.setCustomValidity(``);
-    formValidity = true;
-  }
-});
-
-form.addEventListener(`submit`, (evt) => {
-  evt.preventDefault();
-  if (formValidity) {
-    console.log(`good`);
-  }
-});
-
+if (coachesBlock) {
+  mySwiper(coachesBlock);
+}
 
 const reviewsBlock = document.querySelector(`.reviews-block__slider`);
 
@@ -108,3 +74,46 @@ const mySwiper2 = (el) => {
 if (reviewsBlock) {
   mySwiper2(reviewsBlock);
 }
+
+const scroll = () => {
+  // eslint-disable-next-line no-undef
+  return new SmoothScroll(`a[href*="#"]`, {
+    speed: 500
+  });
+};
+
+scroll();
+
+const phoneInput = document.getElementById(`phone`);
+
+const phoneMask = (el) => {
+  // eslint-disable-next-line no-undef
+  return new IMask(el, {
+    mask: `+{7}(000)000-00-00`,
+    lazy: false
+  });
+};
+
+if (phoneInput) {
+  phoneMask(phoneInput);
+}
+
+let form = document.querySelector(`.contacts-block__form-wrapper form`);
+let formValidity = false;
+phoneInput.setCustomValidity(`Заполните это поле.`);
+
+phoneInput.addEventListener(`input`, () => {
+  if (phoneInput.value.indexOf(`_`) === -1) {
+    phoneInput.setCustomValidity(``);
+    formValidity = true;
+  }
+});
+
+form.addEventListener(`submit`, (evt) => {
+  evt.preventDefault();
+  if (formValidity) {
+    setTimeout(() => {
+      form.reset();
+    }, 750);
+  }
+});
