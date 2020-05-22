d913728d707da89605ddad6577dfeb811eb1c555
Roman Korupaev <rkorupaev@gmail.com>
2020-05-22 20:28:02 +0300

    js for mobile and tablet schedule

--- source/js/modules/index.js
+++ source/js/modules/index.js
@@ -126,6 +126,10 @@ const timeCellsArray = document.querySelectorAll(`.time-list__item`);
 const dayCellsArray = document.querySelectorAll(`.day-list__item`);
 const exerciseListsArray = document.querySelectorAll(`.exercise-list`);
 
+const dropDownArrow = document.querySelector(`.schedule-block__arrow`);
+const dropDownArrowSpan = dropDownArrow.querySelector(`span`);
+const dropDownItemList = document.querySelectorAll(`.drop-down__item`);
+
 if (exerciseListsArray) {
   exerciseListsArray.forEach((list, listIndex) => {
     const itemsArray = list.querySelectorAll(`.exercise-list__item`);
@@ -134,12 +138,14 @@ if (exerciseListsArray) {
         item.classList.add(`exercise-list__item--active`);
         timeCellsArray[itemIndex].classList.add(`time-list__item--active`);
         dayCellsArray[listIndex].classList.add(`day-list__item--active`);
+        dropDownItemList[listIndex].classList.add(`drop-down__item--active-cell`);
       });
 
       item.addEventListener(`mouseout`, (evt) => {
         item.classList.remove(`exercise-list__item--active`);
         timeCellsArray[itemIndex].classList.remove(`time-list__item--active`);
         dayCellsArray[listIndex].classList.remove(`day-list__item--active`);
+        dropDownItemList[listIndex].classList.remove(`drop-down__item--active-cell`);
       })
     });
   });
@@ -156,29 +162,78 @@ const startIndex = [0, 1, 2];
 const endIndex = [4, 5, 6];
 const sliderPin = document.querySelector(`.schedule-block__slider-pin`);
 
-if (sliderPin) {
-  sliderPin.addEventListener(`click`, (evt) => {
-    evt.preventDefault();
-    sliderPin.classList.toggle(`schedule-block__slider-pin--position-right`);
-    let count = 0;
-    exerciseListsArray.forEach((item, itemIndex) => {
-      if (item.classList.contains(`exercise-list--tablet`) && itemIndex >= 4 && count < 3) {
-        count++;
-        exerciseListsArray[itemIndex].classList.remove(`exercise-list--tablet`);
-        addAnimation(exerciseListsArray[3], `unfade`);
-        addAnimation(exerciseListsArray[itemIndex], `unfade`);
-        startIndex.forEach((item) => {
-          exerciseListsArray[item].classList.add(`exercise-list--tablet`);
-        });
-      } else if (item.classList.contains(`exercise-list--tablet`) && itemIndex <= 2 && count < 3) {
-        count++;
-        exerciseListsArray[itemIndex].classList.remove(`exercise-list--tablet`);
-        addAnimation(exerciseListsArray[3], `unfade`);
-        addAnimation(exerciseListsArray[itemIndex], `unfade`);
-        endIndex.forEach((item) => {
-          exerciseListsArray[item].classList.add(`exercise-list--tablet`);
-        });
-      }
+// if (sliderPin) {
+//   sliderPin.addEventListener(`click`, (evt) => {
+//     evt.preventDefault();
+//     // sliderPin.style.left = `100px`;
+//     // sliderPin.classList.toggle(`schedule-block__slider-pin--position-right`);
+//     // let count = 0;
+//     // exerciseListsArray.forEach((item, itemIndex) => {
+//     //   if (item.classList.contains(`exercise-list--tablet`) && itemIndex >= 4 && count < 3) {
+//     //     count++;
+//     //     exerciseListsArray[itemIndex].classList.remove(`exercise-list--tablet`);
+//     //     addAnimation(exerciseListsArray[3], `unfade`);
+//     //     addAnimation(exerciseListsArray[itemIndex], `unfade`);
+//     //     startIndex.forEach((item) => {
+//     //       exerciseListsArray[item].classList.add(`exercise-list--tablet`);
+//     //     });
+//     //   } else if (item.classList.contains(`exercise-list--tablet`) && itemIndex <= 2 && count < 3) {
+//     //     count++;
+//     //     exerciseListsArray[itemIndex].classList.remove(`exercise-list--tablet`);
+//     //     addAnimation(exerciseListsArray[3], `unfade`);
+//     //     addAnimation(exerciseListsArray[itemIndex], `unfade`);
+//     //     endIndex.forEach((item) => {
+//     //       exerciseListsArray[item].classList.add(`exercise-list--tablet`);
+//     //     });
+//     //   }
+//     // });
+//   });
+// }
+
+sliderPin.addEventListener(`mousedown`, (evt) => {
+  const initLocationX = evt.clientX;
+
+  const onMouseMove = (moveEvt) => {
+    moveEvt.preventDefault();
+  }
+});
+
+const removeArrayClass = (block, className) => {
+  block.forEach((item) => {
+    item.classList.remove(className);
+  });
+};
+
+const initDropDown = () => {
+  dropDownArrowSpan.classList.add(`rotate`);
+
+  timeCellsArray.forEach((item) => {
+    item.classList.add(`time-list__item--drop-down-opened`);
+  });
+
+  exerciseListsArray.forEach((item) => {
+    item.classList.add(`exercise-list--drop-down-opened`);
+  });
+
+  dropDownItemList.forEach((item, index) => {
+    item.classList.add(`drop-down__item--active`);
+    item.classList.add(`drop-down__item--border`);
+    dropDownItemList.forEach((item, index) => {
+      item.addEventListener(`click`, (evt) => {
+        removeArrayClass(dropDownItemList, `drop-down__item--active`);
+        removeArrayClass(dropDownItemList, `drop-down__item--border`);
+        dropDownItemList[index].classList.add(`drop-down__item--active`);
+        exerciseListsArray[index].classList.remove(`exercise-list--drop-down-opened`);
+        exerciseListsArray[index].classList.add(`exercise-list--mobile`);
+        removeArrayClass(timeCellsArray, `time-list__item--drop-down-opened`);
+        dropDownArrowSpan.classList.remove(`rotate`);
+        item.style.borderBottomWidth = `2px`;
+      });
+
     });
   });
+};
+
+if (dropDownArrow) {
+  dropDownArrow.addEventListener(`click`, initDropDown);
 }
